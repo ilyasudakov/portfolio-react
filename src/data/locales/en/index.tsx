c681ad26185ce00953ca9ac525416b3532578993
ilyasudakov <ilyasudakov@inbox.ru>
2022-08-29 17:52:55 +0300

    Update text files for en/ru lang

--- src/data/locales/en/index.tsx
+++ src/data/locales/en/index.tsx
@@ -11,144 +11,156 @@ import {
   TITLE_ID
 } from '../types';
 
-const en: landingPageSectionsType = {
-  header: { id: HEADER_ID, sourceCodeLink: 'Source code' },
-  title: {
-    id: TITLE_ID,
-    fullName: 'Ilya Sudakov',
-    position: 'React developer'
-  },
-  about: {
-    id: ABOUT_ID,
-    title: 'About me',
-    text1:
-      'React developer based in Saint-Petersburg, Russia, 24 y.o. Currently \
-      looking for a position as a React/Frontend developer. You can check out ',
-    link1: 'my portfolio',
-    link2: ' download CV.',
-    text2: 'or',
-    text3:
-      'Searching an opportunity to get a real world dev expirience, working \
-      in team, relevant feedback for improving my skills and\
-      acquiring new knowledge. Quick at getting a grip at new things, love to learn new things.'
-  },
-  projects: {
-    id: PROJECTS_ID,
-    title: 'Projects',
-    list: {
-      crm: {
-        title: 'CRM/ERP system',
-        description: 'Frontend for production management system'
-      },
-      'react-base': {
-        title: 'react-base',
-        description: 'Simple template for React projects'
-      },
-      techstack: {
-        title: 'TechStack',
-        description: 'Build your stack using Github repos'
-      },
-      'repo.md': {
-        title: 'repo.md',
-        description: 'Your Github repo as a Markdown blog'
-      },
-      devhub: {
-        title: 'Dev Hub',
-        description:
-          "Productivity and articles by interest. \
-          Todo list, custom user' link list, Medium articles by interest"
-      },
-      'weather-app': {
-        title: 'Weather App',
-        description:
-          "Web app for viewing weather info \
-          by user's location with ability to view forecast for 7 days"
-      },
-      spotify: {
-        title: 'Spotify Statistics',
-        description:
-          'Web app for viewing\
-          favorite songs/artists, and also recomendations list in Spotify'
-      },
-      ocean: {
-        title: 'Ocean',
-        description: 'Simple experiment using ThreeJS'
-      }
+const header: landingPageSectionsType[typeof HEADER_ID] = {
+  id: HEADER_ID,
+  sourceCodeLink: 'Source code'
+};
+const title: landingPageSectionsType[typeof TITLE_ID] = {
+  id: TITLE_ID,
+  fullName: 'Ilya Sudakov',
+  position: 'React developer'
+};
+const about: landingPageSectionsType[typeof ABOUT_ID] = {
+  id: ABOUT_ID,
+  title: 'About me',
+  text1:
+    'React developer based in Saint-Petersburg, Russia, 24 y.o. Currently \
+    looking for a position as a React/Frontend developer. You can check out ',
+  link1: 'my portfolio',
+  link2: ' download CV.',
+  text2: 'or',
+  text3:
+    'Searching an opportunity to get a real world dev expirience, working \
+    in team, relevant feedback for improving my skills and\
+    acquiring new knowledge. Quick at getting a grip at new things, love to learn new things.'
+};
+const projects: landingPageSectionsType[typeof PROJECTS_ID] = {
+  id: PROJECTS_ID,
+  title: 'Projects',
+  list: {
+    crm: {
+      title: 'CRM/ERP system',
+      description: 'Frontend for production management system'
+    },
+    'react-base': {
+      title: 'react-base',
+      description: 'Simple template for React projects'
+    },
+    techstack: {
+      title: 'TechStack',
+      description: 'Build your stack using Github repos'
+    },
+    'repo.md': {
+      title: 'repo.md',
+      description: 'Your Github repo as a Markdown blog'
+    },
+    devhub: {
+      title: 'Dev Hub',
+      description:
+        "Productivity and articles by interest. \
+        Todo list, custom user' link list, Medium articles by interest"
+    },
+    'weather-app': {
+      title: 'Weather App',
+      description:
+        "Web app for viewing weather info \
+        by user's location with ability to view forecast for 7 days"
+    },
+    spotify: {
+      title: 'Spotify Statistics',
+      description:
+        'Web app for viewing\
+        favorite songs/artists, and also recomendations list in Spotify'
+    },
+    ocean: {
+      title: 'Ocean',
+      description: 'Simple experiment using ThreeJS'
     }
-  },
-  bio: {
-    id: BIO_ID,
-    title: 'Bio',
-    list: [
-      {
-        period: <span>2016 — 2020</span>,
-        description:
-          'Software Engineer. The Bonch-Bruevich Saint Petersburg State University of Telecommunications. Bachelor degree'
-      },
-      {
-        period: (
-          <span>
-            2020 — 2021 <SmallTextPeriod>march</SmallTextPeriod>
-          </span>
-        ),
-        description:
-          "Company 'Osfix'. Frontend developer of CRM/ERP-system, used on small production factory."
-      },
-      {
-        period: (
-          <span>
-            2021 <SmallTextPeriod>may</SmallTextPeriod> — 2022{' '}
-            <SmallTextPeriod>may</SmallTextPeriod>
-          </span>
-        ),
-        description: 'Service on conscription'
-      }
-    ]
-  },
-
-  skills: {
-    id: SKILLS_ID,
-    title: 'Skills',
-    list: [
-      {
-        setName: 'Frontend',
-        skills: [
-          'React',
-          'Context API',
-          'JavaScript',
-          'TypeScript',
-          'NextJS',
-          'Redux',
-          'Redux Toolkit',
-          'RTK Query',
-          'TailwindCSS',
-          'NextAuth',
-          'CSS/SCSS',
-          'HTML',
-          'API',
-          'Webpack',
-          'Vite'
-        ]
-      },
-      {
-        setName: 'Tools',
-        skills: ['Git', 'GitHub', 'Github Actions', 'Figma', 'Trello']
-      },
-      {
-        setName: 'Languages',
-        skills: ['Russian - native', 'English - B1']
-      }
-    ]
-  },
-  contacts: {
-    id: CONTACTS_ID,
-    title: 'Contacts'
-  },
-  footer: {
-    id: FOOTER_ID,
-    madeBy: '© 2022 Ilya Sudakov.',
-    madeWith: 'Made with NextJS, TypeScript, Tailwind.'
   }
+};
+const bio: landingPageSectionsType[typeof BIO_ID] = {
+  id: BIO_ID,
+  title: 'Bio',
+  list: [
+    {
+      period: <span>2016 — 2020</span>,
+      description:
+        'Software Engineer. The Bonch-Bruevich Saint Petersburg State University of Telecommunications. Bachelor degree'
+    },
+    {
+      period: (
+        <span>
+          2020 — 2021 <SmallTextPeriod>march</SmallTextPeriod>
+        </span>
+      ),
+      description:
+        "Company 'Osfix'. Frontend developer of CRM/ERP-system, used on small production factory."
+    },
+    {
+      period: (
+        <span>
+          2021 <SmallTextPeriod>may</SmallTextPeriod> — 2022{' '}
+          <SmallTextPeriod>may</SmallTextPeriod>
+        </span>
+      ),
+      description: 'Service on conscription'
+    }
+  ]
+};
+
+const skills: landingPageSectionsType[typeof SKILLS_ID] = {
+  id: SKILLS_ID,
+  title: 'Skills',
+  list: [
+    {
+      setName: 'Frontend',
+      skills: [
+        'React',
+        'Context API',
+        'JavaScript',
+        'TypeScript',
+        'NextJS',
+        'Redux',
+        'Jest/RTL',
+        'Redux Toolkit',
+        'RTK Query',
+        'TailwindCSS',
+        'NextAuth',
+        'CSS/SCSS',
+        'HTML',
+        'API',
+        'Webpack',
+        'Vite'
+      ]
+    },
+    {
+      setName: 'Tools',
+      skills: ['Git', 'GitHub', 'Github Actions', 'Figma', 'Trello']
+    },
+    {
+      setName: 'Languages',
+      skills: ['Russian - native', 'English - B2']
+    }
+  ]
+};
+const contacts: landingPageSectionsType[typeof CONTACTS_ID] = {
+  id: CONTACTS_ID,
+  title: 'Contacts'
+};
+const footer: landingPageSectionsType[typeof FOOTER_ID] = {
+  id: FOOTER_ID,
+  madeBy: '© 2022 Ilya Sudakov.',
+  madeWith: 'Made with NextJS, TypeScript, Tailwind.'
+};
+const en: landingPageSectionsType = {
+  header,
+  title,
+  about,
+  projects,
+  bio,
+  skills,
+  contacts,
+  footer
 };
 
 export default en;
--- src/data/locales/ru/index.tsx
+++ src/data/locales/ru/index.tsx
@@ -11,142 +11,156 @@ import {
   TITLE_ID
 } from '../types';
 
-const ru: landingPageSectionsType = {
-  header: { id: HEADER_ID, sourceCodeLink: 'Посмотреть код' },
-  title: {
-    id: TITLE_ID,
-    fullName: 'Илья Судаков',
-    position: 'React разработчик'
-  },
-  about: {
-    id: ABOUT_ID,
-    title: 'Обо мне',
-    text1:
-      'React-разработчик из Санкт-Петербурга, 24 года. В данный момент ищу \
-    работу по вакансии junior-разработчик. Вы можете посмотреть',
-    link1: 'мое портфолио',
-    link2: ' скачать CV.',
-    text2: 'или',
-    text3:
-      'Ищу возможности получения реального опыта разработки, работы в\
-    команде, полезного фидбека для улучшения своих навыков и получения\
-    новых знаний. Быстро обучаем, люблю учиться.'
-  },
-  projects: {
-    id: PROJECTS_ID,
-    title: 'Проекты',
-    list: {
-      crm: {
-        title: 'CRM/ERP система',
-        description: 'Фронтенд для системы управления малым предприятием'
-      },
-      'react-base': {
-        title: 'react-base',
-        description: 'Простой шаблон для React-проектов'
-      },
-      techstack: {
-        title: 'TechStack',
-        description: 'Соберите свой стек из Github репозиториев'
-      },
-      'repo.md': {
-        title: 'repo.md',
-        description: 'Ваш Github репозиторий как Markdown блог'
-      },
-      devhub: {
-        title: 'Dev Hub',
-        description:
-          'Продуктивность и тематические статьи на одном экране. \
-        Todo list, список кастомных пользовательских ссылок, Тематические статьи Medium'
-      },
-      'weather-app': {
-        title: 'Weather App',
-        description:
-          'Приложение для просмотра погоды по \
-          геолокации пользователя, с возможностью просмотра прогноза на 7 дней'
-      },
-      spotify: {
-        title: 'Spotify Statistics',
-        description:
-          'Приложение для просмотра\
-         любимых песен/исполнителей, а также получение списка рекомендаций в Spotify'
-      },
-      ocean: {
-        title: 'Ocean',
-        description: 'Простой эксперимент с использованием ThreeJS'
-      }
+const header: landingPageSectionsType[typeof HEADER_ID] = {
+  id: HEADER_ID,
+  sourceCodeLink: 'Посмотреть код'
+};
+const title: landingPageSectionsType[typeof TITLE_ID] = {
+  id: TITLE_ID,
+  fullName: 'Илья Судаков',
+  position: 'React разработчик'
+};
+const about: landingPageSectionsType[typeof ABOUT_ID] = {
+  id: ABOUT_ID,
+  title: 'Обо мне',
+  text1:
+    'React-разработчик из Санкт-Петербурга, 24 года. В данный момент ищу \
+  работу по вакансии junior-разработчик. Вы можете посмотреть ',
+  link1: 'мое портфолио',
+  link2: ' скачать CV.',
+  text2: 'или',
+  text3:
+    'Ищу возможности получения реального опыта разработки, работы в\
+  команде, полезного фидбека для улучшения своих навыков и получения\
+  новых знаний. Быстро обучаем, люблю учиться.'
+};
+const projects: landingPageSectionsType[typeof PROJECTS_ID] = {
+  id: PROJECTS_ID,
+  title: 'Проекты',
+  list: {
+    crm: {
+      title: 'CRM/ERP система',
+      description: 'Фронтенд для системы управления малым предприятием'
+    },
+    'react-base': {
+      title: 'react-base',
+      description: 'Простой шаблон для React-проектов'
+    },
+    techstack: {
+      title: 'TechStack',
+      description: 'Соберите свой стек из Github репозиториев'
+    },
+    'repo.md': {
+      title: 'repo.md',
+      description: 'Ваш Github репозиторий как Markdown блог'
+    },
+    devhub: {
+      title: 'Dev Hub',
+      description:
+        'Продуктивность и тематические статьи на одном экране. \
+      Todo list, список кастомных пользовательских ссылок, Тематические статьи Medium'
+    },
+    'weather-app': {
+      title: 'Weather App',
+      description:
+        'Приложение для просмотра погоды по \
+        геолокации пользователя, с возможностью просмотра прогноза на 7 дней'
+    },
+    spotify: {
+      title: 'Spotify Statistics',
+      description:
+        'Приложение для просмотра\
+       любимых песен/исполнителей, а также получение списка рекомендаций в Spotify'
+    },
+    ocean: {
+      title: 'Ocean',
+      description: 'Простой эксперимент с использованием ThreeJS'
     }
-  },
-  bio: {
-    id: BIO_ID,
-    title: 'Биография',
-    list: [
-      {
-        period: <span>2016 — 2020</span>,
-        description:
-          'Программная инженерия. Санкт-Петербургский Государственный Университет Телекоммуникаций им. М.А. Бонч-Бруевича. Бакалавр'
-      },
-      {
-        period: (
-          <span>
-            2020 — 2021 <SmallTextPeriod>март</SmallTextPeriod>
-          </span>
-        ),
-        description:
-          'Компания Osfix. Фронтенд-разработчик CRM/ERP-системы, используемой на малом предприятии.'
-      },
-      {
-        period: (
-          <span>
-            2021 <SmallTextPeriod>май</SmallTextPeriod> — 2022{' '}
-            <SmallTextPeriod>май</SmallTextPeriod>
-          </span>
-        ),
-        description: 'Служба по призыву'
-      }
-    ]
-  },
-  skills: {
-    id: SKILLS_ID,
-    title: 'Навыки',
-    list: [
-      {
-        setName: 'Фронтенд',
-        skills: [
-          'React',
-          'Context API',
-          'JavaScript',
-          'TypeScript',
-          'NextJS',
-          'Redux',
-          'Redux Toolkit',
-          'RTK Query',
-          'TailwindCSS',
-          'NextAuth',
-          'CSS/SCSS',
-          'HTML',
-          'Работа с API',
-          'Webpack',
-          'Vite'
-        ]
-      },
-      {
-        setName: 'Инструменты',
-        skills: ['Git', 'GitHub', 'Github Actions', 'Figma', 'Trello']
-      },
-      {
-        setName: 'Языки',
-        skills: ['Русский - родной', 'Английский - B1']
-      }
-    ]
-  },
-  contacts: {
-    id: CONTACTS_ID,
-    title: 'Контакты'
-  },
-  footer: {
-    id: FOOTER_ID,
-    madeBy: '© 2022 Илья Судаков.',
-    madeWith: 'Сделано с NextJS, TypeScript, Tailwind.'
   }
+};
+const bio: landingPageSectionsType[typeof BIO_ID] = {
+  id: BIO_ID,
+  title: 'Биография',
+  list: [
+    {
+      period: <span>2016 — 2020</span>,
+      description:
+        'Программная инженерия. Санкт-Петербургский Государственный Университет Телекоммуникаций им. М.А. Бонч-Бруевича. Бакалавр'
+    },
+    {
+      period: (
+        <span>
+          2020 — 2021 <SmallTextPeriod>март</SmallTextPeriod>
+        </span>
+      ),
+      description:
+        'Компания Osfix. Фронтенд-разработчик CRM/ERP-системы, используемой на малом предприятии.'
+    },
+    {
+      period: (
+        <span>
+          2021 <SmallTextPeriod>май</SmallTextPeriod> — 2022{' '}
+          <SmallTextPeriod>май</SmallTextPeriod>
+        </span>
+      ),
+      description: 'Служба по призыву'
+    }
+  ]
+};
+
+const skills: landingPageSectionsType[typeof SKILLS_ID] = {
+  id: SKILLS_ID,
+  title: 'Навыки',
+  list: [
+    {
+      setName: 'Фронтенд',
+      skills: [
+        'React',
+        'Context API',
+        'JavaScript',
+        'TypeScript',
+        'NextJS',
+        'Redux',
+        'Redux Toolkit',
+        'RTK Query',
+        'TailwindCSS',
+        'Jest/RTL',
+        'NextAuth',
+        'CSS/SCSS',
+        'HTML',
+        'Работа с API',
+        'Webpack',
+        'Vite'
+      ]
+    },
+    {
+      setName: 'Инструменты',
+      skills: ['Git', 'GitHub', 'Github Actions', 'Figma', 'Trello']
+    },
+    {
+      setName: 'Языки',
+      skills: ['Русский - родной', 'Английский - B2']
+    }
+  ]
+};
+const contacts: landingPageSectionsType[typeof CONTACTS_ID] = {
+  id: CONTACTS_ID,
+  title: 'Контакты'
+};
+const footer: landingPageSectionsType[typeof FOOTER_ID] = {
+  id: FOOTER_ID,
+  madeBy: '© 2022 Илья Судаков.',
+  madeWith: 'Сделано с NextJS, TypeScript, Tailwind.'
+};
+
+const ru: landingPageSectionsType = {
+  header,
+  title,
+  about,
+  projects,
+  bio,
+  skills,
+  contacts,
+  footer
 };
 export default ru;
